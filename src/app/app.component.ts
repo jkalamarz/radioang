85dacdad87e8e0322be4798bf2ed56a6fccfe3a4
jkalamarz <jkalamarz@gmail.com>
2018-11-18 00:50:19 -0800

    Minor update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+
+
 })
+
+class Radio {
+  id: number;
+  name: string;
+}
+
 export class AppComponent {
   title = 'My app';
   d = [1, 2, 6, 3];
+}
+
+export class CustomerComponent implements OnInit {
+  radios : Observable<Radio[]>;
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit() {
+      this.radios = this.http
+          .get<Radio[]>("/radioapi/values");
+  }
 }
